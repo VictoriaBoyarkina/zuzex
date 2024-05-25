c80fa8eeedcacdc6ca85a3990a6da3d8951deea9
VictoriaBoyarkina <victoriaboyarkina@gmail.com>
2024-05-26 00:42:11 +0300

    Add layouts

--- frontend/src/RunApp.js
+++ frontend/src/RunApp.js
@@ -1,33 +1,7 @@
-import { io } from "socket.io-client";
 import App from "./components/App.js";
 
 const RunApp = () => {
-  const socket = io();
-
-  // socket.on("newMessage", (message) => {
-  //   dispatch(messagesActions.addMessage(message));
-  // });
-
-  // socket.on("newChannel", (channel) => {
-  //   dispatch(channelsActions.addChannel(channel));
-  // });
-
-  // socket.on("removeChannel", (channel) => {
-  //   dispatch(channelsActions.removeChannel(channel.id));
-  // });
-
-  // socket.on("renameChannel", (channel) => {
-  //   dispatch(
-  //     channelsActions.updateChannel({
-  //       id: channel.id,
-  //       changes: {
-  //         name: channel.name,
-  //       },
-  //     })
-  //   );
-  // });
-
-  return <App socket={socket} />;
+  return <App/>;
 };
 
 export default RunApp;
